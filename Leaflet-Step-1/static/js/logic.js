464fca87b107f787ff5efdc4ef49cd46190d975b
Shay O'Connell <shay.oconnell7@gmail.com>
2021-01-04 21:29:33 -0500

    fixed legend

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,96 +1,89 @@
-
-// var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=1994-12-01&endtime=2014-12-01";
 var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2020-07-01&endtime=" +
-  "2020-07-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
-
+  "2020-07-31&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
 d3.json(queryUrl, function(data) {
-//   createFeatures(data.features);
+    createFeatures(data.features);
   console.log(data);
-
-  L.geoJson(data,{
-      pointToLayer: function(feature, latlng){
-          return L.circleMarker(latlng);
-      },
-    //   style: styleInfo,
-      onEachFeature: function(feature, layer){
-          layer.bindPopup(
-              "Magnitude: "
-              + feature.properties.mag
-              + "<br>Depth: "
-              + feature.geometry.coordinates[2]
-              +"<br>Location "
-              +feature.properties.place
-          );
-      }
-  }).addTo(myMap)
 });
+function createFeatures(earthquakeData) {
+    function styleInfo(feature) {
+        return {
+        opacity: 1,
+        fillOpacity: .5,
+        fillColor: getColor(feature.properties.mag),
+        radius: getRadius(feature.properties.mag),
+        stroke: true,
+        weight: 0.5
+        };
+    };
+    function getColor(magnitude) {
+        switch (true){
+            case magnitude > 5:
+                return "#bc0000";
+            case magnitude > 4:
+                return "#c6060e";
+            case magnitude > 3:
+                return "#ce131a";
+            case magnitude > 2:
+                return "#db2f2d";
+            case magnitude > 1:
+                return "#dd4035"; 
+            default:
+                return "#d26740";
+        }
+    };
+        function getRadius(magnitude){
+            return (magnitude*6)
+        };
+    var earthquakes = L.geoJson(earthquakeData,{
+        pointToLayer: function(feature, latlng){
+            return L.circleMarker(latlng);
+      },
+    style: styleInfo,
+    onEachFeature: function(feature, layer){
+        layer.bindPopup(
+            "Magnitude: "
+            + feature.properties.mag
+            + "<br>Depth: "
+            + feature.geometry.coordinates[2]
+            +"<br>Location "
+            + feature.properties.place
+            +"<br>Time "
+            + Date(feature.properties.time)
+        );
+      },
+    });
+    
+  var overlayMaps = {
+    Earthquakes: earthquakes
+  };
 
-// function styleInfo(feature) {
-//     return {
-//       opacity: 1,
-//       fillOpacity: 1,
-//       fillColor: getColor(feature.geometry.coordinates[2]),
-//       color: "#000000",
-//       radius: getRadius(feature.properties.mag),
-//       stroke: true,
-//       weight: 0.5
-//     };
-//   };
+var map = L.map("map", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5,
+    layers: [streetmap, earthquakes]
+  });
 
+L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(map);
 
-// function createFeatures(earthquakeData) {
-
-//   function onEachFeature(feature, circle) {
-//     var circle = L.circleMarker([feature.geometry.coordinates[0],feature.geometry.coordinates[1]],{
-//         color: 'red',
-//         fillColor: '#f03',
-//         fillOpacity: 0.5,
-//         radius: feature.properties.mag*3,
-//     });
-//     circle.bindPopup("<h3>" + feature.properties.place +
-//       "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-
-//   };
-//   var earthquakes = L.geoJSON(earthquakeData, {
-//     onEachFeature: onEachFeature
-//   });
-  
-
-//   createMap(earthquakes);
-// }
-
-function createMap(earthquakes) {
-
-
-   
-
-//     // # first cut the continuous variable into bins
-//     // # these bins are now factors
-//     magnitudes <- cut(last$BeatHome, 
-//                         c(2.5,5.4,6.0,6.9,7.9,100), include.lowest = T,
-//                         labels = c('<2.5', '2.5-5.4', '5.5-6.0', '6.0-6.9', '7-7.9','8+'))
-
-// // # then assign a palette to this using colorFactor
-// // # in this case it goes from red for the smaller values to yellow and green
-// // # standard stoplight for bad, good, and best
-//     magCol <- colorFactor(palette = 'RdYlGn', magnitudes)
-
-
-//     m1 <- leaflet() %>%
-//         addTiles() %>%
-//         addProviderTiles(providers$OpenStreetMap, group = 'Open SM')  %>%
-//         addProviderTiles(providers$Stamen.Toner, group = 'Toner')  %>%
-//         addProviderTiles(providers$Esri.NatGeoWorldMap, group = 'NG World') %>%
-//         setView(lng = -72, lat = 41, zoom = 8) %>%
-
-//       addCircleMarkers(data = Jun, lat = ~Lat, lng = ~Lon,
-//                        color = ~beatCol(BeatHomeLvl), popup = Jun$Popup,
-//                        radius = ~sqrt(BeatHome*50), group = 'Home - Jun') %>%
-
-
-//                        addLegend('bottomright', pal = magCol, values = magnitudes,
-//                        title = 'Earthquake Magnitudes',
-//                        opacity = 1)
+    var legend = L.control({position:'bottomright'});
+    legend.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 1, 2, 3, 4, 5],
+        labels = ["<1", ">1", ">2", ">3", ">4", ">5" ];
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            'Magnitude <i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br><br>' : '+');
+    }
+    return div;
+    };
+    legend.addTo(map);
+}
 
   var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
@@ -101,27 +94,156 @@ function createMap(earthquakes) {
     accessToken: API_KEY
   });
 
-
   var baseMaps = {
     "Street Map": streetmap
   };
+  
 
-  var overlayMaps = {
-    Earthquakes: earthquakes
-  };
 
-  var myMap = L.map("map", {
-    center: [
-      37.09, -95.71
-    ],
-    zoom: 5,
-    layers: [streetmap, earthquakes]
-  });
 
-//   Create a layer control
-//   Pass in our baseMaps and overlayMaps
-//   Add the layer control to the map
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(myMap);
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2020-07-01&endtime=" +
+//   "2020-07-31&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
+
+// d3.json(queryUrl, function(data) {
+//     createFeatures(data.features);
+//   console.log(data);
+// });
+
+
+
+// function createFeatures(earthquakeData) {
+
+    
+
+//     function styleInfo(feature) {
+//         return {
+//         opacity: 1,
+//         fillOpacity: .5,
+//         fillColor: getColor(feature.properties.mag),
+//         radius: getRadius(feature.properties.mag),
+//         stroke: true,
+//         weight: 0.5
+//         };
+//     };
+//     function getColor(magnitude) {
+//         switch (true){
+//             case magnitude > 5:
+//                 return "#bc0000";
+//             case magnitude > 4:
+//                 return "#c6060e";
+//             case magnitude > 3:
+//                 return "#ce131a";
+//             case magnitude > 2:
+//                 return "#db2f2d";
+//             case magnitude > 1:
+//                 return "#dd4035"; 
+//             default:
+//                 return "#d26740";
+//         }
+//     };
+//         function getRadius(magnitude){
+//             return (magnitude*6)
+//         };
+
+    
+//     var earthquakes = L.geoJson(earthquakeData,{
+//         pointToLayer: function(feature, latlng){
+//             return L.circleMarker(latlng);
+//       },
+//     style: styleInfo,
+//     onEachFeature: function(feature, layer){
+//         layer.bindPopup(
+//             "Magnitude: "
+//             + feature.properties.mag
+//             + "<br>Depth: "
+//             + feature.geometry.coordinates[2]
+//             +"<br>Location "
+//             + feature.properties.place
+//             +"<br>Time "
+//             + Date(feature.properties.time)
+//         );
+//       },
+
+//     });
+
+// // createMap(earthquakes);
+
+//     var legend = L.control({position:'bottomleft'});
+
+//     legend.onAdd = function() {
+
+//     var div = L.DomUtil.create('div', 'info legend'),
+//         grades = [0, 1, 2, 3, 4, 5, ],
+//         labels = ["<1", ">1", ">2", ">3", ">4", ">5" ];
+
+//     // loop through our density intervals and generate a label with a colored square for each interval
+//     for (var i = 0; i < grades.length; i++) {
+//         div.innerHTML +=
+//             '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+//             grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+//     }
+
+//     return div;
+//     };
+
+//     legend.addTo(map);
+   
+
+// }
+
+// var map = L.map("map", {
+//     center: [
+//       37.09, -95.71
+//     ],
+//     zoom: 5,
+//     layers: [streetmap, earthquakes]
+//   });
+// var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+//     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+//     tileSize: 512,
+//     maxZoom: 18,
+//     zoomOffset: -1,
+//     id: "mapbox/streets-v11",
+//     accessToken: API_KEY
+//   });
+
+// var baseMaps = {
+//     "Street Map": streetmap
+//   };
+
+// var overlayMaps = {
+//     Earthquakes: earthquakes
+//   };
+
+
+
+// L.control.layers(baseMaps, overlayMaps, {
+//     collapsed: false
+//   }).addTo(map);
+
+
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# leaflet-challenge
+
+1. **Get the data set**
+
+   The USGS provides earthquake data in a number of different formats, updated every 5 minutes. I chose a dataset from [USGS GeoJSON Feed](http://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php) to visualize.
+
+2. **Import & Visualize the Data**
+
+   I created a map using Leaflet that plots all of the earthquakes from my chosen data set based on their longitude and latitude.
+
+   * The data markers reflect the magnitude of the earthquake in their size and color. Earthquakes with higher magnitudes appear larger and darker in color.
+
+   * I also included popups that provides additional information about the earthquake when a marker is clicked.
+
+   * I created a legend that provides context for the magnitude coloration map data.
